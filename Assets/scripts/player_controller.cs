279130db4f494a77f617e7555353c1f64e897eee
Ryu_Devloper <k2511076>
2025-11-07 21:36:47 +0900

    Mapに変更加えた。BGM追加した。スプライト作った。

--- Assets/scripts/player_controller.cs
+++ Assets/scripts/player_controller.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class player_controller : MonoBehaviour
 {
+    public float speed = 2.5f;
+    Rigidbody2D rb;
+    Vector2 move;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -13,7 +17,11 @@ public class player_controller : MonoBehaviour
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
+        move = new Vector2(x, y);
+    }
 
-        transform.position += new Vector3(x*Time.deltaTime*2, y*Time.deltaTime*2);
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + move.normalized * speed * Time.fixedDeltaTime);
     }
 }
