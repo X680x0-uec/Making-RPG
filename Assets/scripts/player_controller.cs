5388ae54c4e08af627fd35dca8dfca1a54699789
Ryu_Devloper <k2511076>
2025-11-08 02:07:40 +0900

    アニメーションを追加した。ただ、player_encountが動かなくなったので改良する後で。

--- Assets/scripts/player_controller.cs
+++ Assets/scripts/player_controller.cs
@@ -1,16 +1,22 @@
-using UnityEngine;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class player_controller : MonoBehaviour
 {
     public float speed = 2.5f;
-    Rigidbody2D rb;
-    Vector2 move;
+    private Animator animator;
+    private Rigidbody2D rb;
+    private Vector2 move;
+    private int idX = Animator.StringToHash("x"), idY = Animator.StringToHash("y");
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -18,7 +24,20 @@ public class player_controller : MonoBehaviour
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
-        move = new Vector2(Math.Abs(x) >= Math.Abs(y) ? x : 0, Math.Abs(y) > Math.Abs(x) ? y : 0);
+        x = Math.Abs(x) >= Math.Abs(y) ? x : 0;
+        y = Math.Abs(y) > Math.Abs(x) ? y : 0;
+
+        if (Mathf.FloorToInt(x) != 0 || Mathf.FloorToInt(y) != 0) 
+        {   
+            animator.speed = 1;
+            animator.SetFloat (idX, x);
+			animator.SetFloat (idY, y);
+        }
+        else
+        {
+            animator.speed = 0;
+        }
+        move = new Vector2(x, y);
     }
 
     void FixedUpdate()
