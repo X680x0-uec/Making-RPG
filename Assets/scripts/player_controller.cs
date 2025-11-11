b41727c58318716bb93667c352c188b0a8b848bf
Ryu_Devloper <k2511076>
2025-11-11 17:16:47 +0900

    Encountに関する変更を加えた

--- Assets/scripts/player_controller.cs
+++ Assets/scripts/player_controller.cs
@@ -2,12 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Animator))]
+
 public class player_controller : MonoBehaviour
 {
-    public float speed = 2.5f;
+    [SerializeField] private GameObject UI_Encount;
     private Animator animator;
+    [SerializeField] Animator cameraAnimator;  // カメラワーク
+
+    [SerializeField] public float speed_const = 2.5f;
+    public float speed;
+    public bool stop = false;
     private Rigidbody2D rb;
     private Vector2 move;
     private int idX = Animator.StringToHash("x"), idY = Animator.StringToHash("y");
@@ -15,8 +22,9 @@ public class player_controller : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        UI_Encount.SetActive(false);
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -27,21 +35,64 @@ public class player_controller : MonoBehaviour
         x = Math.Abs(x) >= Math.Abs(y) ? x : 0;
         y = Math.Abs(y) > Math.Abs(x) ? y : 0;
 
-        if ((Mathf.FloorToInt(x) != 0 || Mathf.FloorToInt(y) != 0) && Time.timeScale != 0f)
+        if ((Mathf.FloorToInt(x) != 0 || Mathf.FloorToInt(y) != 0) && Time.timeScale != 0f && !this.stop)
         {   
-            animator.speed = 1;
-            animator.SetFloat (idX, x);
-			animator.SetFloat (idY, y);
+            this.speed = this.speed_const;
+            this.animator.speed = 1;
+            this.animator.SetFloat (idX, x);
+			this.animator.SetFloat (idY, y);
         }
         else
-        {
-            animator.speed = 0;
+        {   
+            this.speed = 0;
+            this.animator.speed = 0;
         }
-        move = new Vector2(x, y);
+        this.move = new Vector2(x, y);
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {   
+        if (collision.tag == "encounter")
+        {
+            int randomencount = UnityEngine.Random.Range(0, 100);
+            if (randomencount == 0)
+            {
+                UI_Encount.SetActive(true);
+                stop = true;
+                Debug.Log("ok");
+                cameraAnimator.SetTrigger("Encounter");
+                StartCoroutine("WAITTIME");
+                int randomenemy = UnityEngine.Random.Range(0, 3);
+                if (randomenemy == 0)
+                {
+                    Static.enemynumber = 0;
+                }
+                if (randomenemy == 1)
+                {
+                    Static.enemynumber = 1;
+                }
+                if (randomenemy == 2)
+                {
+                    Static.enemynumber = 2;
+                }
+            }
+        }
+        if(collision.tag == "danpen")
+        {
+            UI_Encount.SetActive(true);
+            animator.SetTrigger("encount");
+            StartCoroutine("WAITTIME");
+            Static.enemynumber = 10;
+        }
+    }
+    IEnumerator WAITTIME()
+    {
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene("Battle");
     }
 }
