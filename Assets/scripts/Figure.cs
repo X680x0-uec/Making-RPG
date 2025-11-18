bc7cdda1fd454f4d414c66c0e5c952e1227b4ef8
Ryu_Devloper <k2511076>
2025-11-18 23:56:33 +0900

    Battleの変更

--- Assets/scripts/Figure.cs
+++ Assets/scripts/Figure.cs
@@ -41,19 +41,18 @@ public abstract class Figure : MonoBehaviour
     protected virtual void Awake()
     {
         currentHP = maxHP;
-        currentMP = maxMP;
     }
 
     //素早さバフの適用メソッド
     public void ApplySpeedBoost(float multiplier, int duration)
     {
-        this.speedMultiplier = multiplier;
-        this.speedBoostTurns = duration;
+        float speedMultiplier = multiplier;
+        int speedBoostTurns = duration;
         Debug.Log($"{charaName}の素早さが上昇した！");
     }
 
     //素早さバフのターン経過と解除の処理メソッド
-    public virtual void DecrementSpeedBuffTurns()
+    public void DecrementSpeedBuffTurns()
     {
         if (speedBoostTurns > 0)
         {
@@ -71,53 +70,18 @@ public abstract class Figure : MonoBehaviour
     {
         float effectiveDamage = Mathf.Max(0, damage - EffectiveDefense);
         currentHP -= effectiveDamage;
-
-        if ( currentHP <= 0 ) 
-        {
-            currentHP = 0;
-            isDead = true;
-            OnDied?.Invoke();
-            Die();
+        if ( currentHP <= 0 ) { currentHP = 0; isDead = true; }
+        if (OnHPChanged != null) {
+            OnHPChanged.Invoke(currentHP, maxHP);
         }
-
-        //HP変更イベントの呼び出し
-        OnHPChanged?.Invoke(currentHP,maxHP);
-
-        return effectiveDamage; //実際に受けたダメージを返す
-    }
-
-    public virtual float HealHP(float amount)
-    {
-        float healAmount = Mathf.Min(amount, maxHP - currentHP);//最大HPを超えないように調整
-        currentHP += healAmount;
-        OnHPChanged?.Invoke(currentHP, maxHP);
-        return healAmount;
-    }
-
-    public virtual float HealMP(float amount)
-    {
-        float healAmount = Mathf.Min(amount, maxMP - currentMP);
-        currentMP += healAmount;
-        OnMPChanged?.Invoke(currentMP,maxMP);
-        return healAmount;
-    }
-
-    public bool ConsumeMP(float amount)
-    {
-        if(currentMP >= amount)
-        {
-            currentMP -= amount;
-            OnMPChanged?.Invoke(currentMP, maxMP);
-            return true;
-        }
-        return false;
+        return effectiveDamage;
     }
 
 
-    //死亡時の処理（抽象的に指定して、継承先で具体的に実装する）
-    protected abstract void Die();
+//死亡時の処理（抽象的に指定して、継承先で具体的に実装する）
+protected abstract void Die();
 
-    //前バフのターンを減らす処理
-    //public abstract void DecrementBuffTurns();
+//前場符のターンを減らす処理
+public abstract void DecrementBuffTurns();
 
 }
